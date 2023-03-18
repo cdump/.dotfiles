766f03ee69bbc4788e2e0cceec5245930cf2f7f1
Maxim Andreev <andreevmaxim@gmail.com>
2023-03-18 20:37:08 +0300

    vim: IndentBlanklineChar color

--- vim/.config/nvim/lua/colorscheme.lua
+++ vim/.config/nvim/lua/colorscheme.lua
@@ -10,7 +10,7 @@ vim.cmd [[
     hi NvimTreeWinSeparator guifg=#222327 guibg=#222327
     hi NvimTreeCursorLine guibg=#383838
 
-    hi IndentBlanklineChar guifg=#31353d
+    hi IndentBlanklineChar guifg=#282c34
 
     hi BufferLineBufferSelected guifg=#e2e2e3 guibg=#1e222a gui=bold
 
