5d0dea46105a724291667b98a4a707c2e8782b92
Maxim Andreev <andreevmaxim@gmail.com>
2023-09-10 13:41:21 +0300

    neovim: trying mini.clue

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -18,11 +18,6 @@ return require('lazy').setup({
     },
 
     {
-        'rcarriga/nvim-dap-ui',
-        dependencies = {'mfussenegger/nvim-dap'}
-    },
-
-    {
         'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
         keys = {
             { '<leader>l', function() require('lsp_lines').toggle() end, desc = 'Toggle lsp_lines' },
@@ -66,6 +61,48 @@ return require('lazy').setup({
                         opts.split_pattern = '\\%s*$'
                         table.insert(steps.pre_justify, minialign.gen_step.trim())
                     end
+                },
+            }
+
+            local miniclue = require('mini.clue')
+            miniclue.setup {
+                triggers = {
+                    -- Leader triggers
+                    { mode = 'n', keys = '<Leader>' },
+                    { mode = 'x', keys = '<Leader>' },
+
+                    -- `g` key
+                    { mode = 'n', keys = 'g' },
+                    { mode = 'x', keys = 'g' },
+
+                    -- Marks
+                    { mode = 'n', keys = "'" },
+                    { mode = 'n', keys = '`' },
+                    { mode = 'x', keys = "'" },
+                    { mode = 'x', keys = '`' },
+
+                    -- Registers
+                    { mode = 'n', keys = '"' },
+                    { mode = 'x', keys = '"' },
+                    { mode = 'i', keys = '<C-r>' },
+                    { mode = 'c', keys = '<C-r>' },
+
+                    -- Window commands
+                    { mode = 'n', keys = '<C-w>' },
+
+                    -- `z` key
+                    { mode = 'n', keys = 'z' },
+                    { mode = 'x', keys = 'z' },
+                },
+
+                clues = {
+                    -- Enhance this by adding descriptions for <Leader> mapping groups
+                    miniclue.gen_clues.builtin_completion(),
+                    miniclue.gen_clues.g(),
+                    miniclue.gen_clues.marks(),
+                    miniclue.gen_clues.registers(),
+                    miniclue.gen_clues.windows(),
+                    miniclue.gen_clues.z(),
                 },
             }
         end
