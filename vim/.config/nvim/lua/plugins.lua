33a95772c5d1d7f2374f637f052b191ff6449ef5
Maxim Andreev <andreevmaxim@gmail.com>
2023-09-19 10:41:22 +0300

    vim: eslint_d->prettier for js code format

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -456,7 +456,7 @@ return require('lazy').setup({
                 sources = {
                     null_ls.builtins.diagnostics.eslint_d,
                     null_ls.builtins.code_actions.eslint_d,
-                    null_ls.builtins.formatting.eslint_d,
+                    null_ls.builtins.formatting.prettier,
 
                     null_ls.builtins.formatting.black.with {
                         only_local = '.venv/bin',
