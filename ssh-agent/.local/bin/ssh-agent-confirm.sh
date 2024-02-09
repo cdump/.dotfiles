618777a4c6113bdfc84b5016417632ec2c688a67
Maxim Andreev <andreevmaxim@gmail.com>
2024-02-09 09:55:30 +0300

    ssh-agent: show key fingerprint

--- ssh-agent/.local/bin/ssh-agent-confirm.sh
+++ ssh-agent/.local/bin/ssh-agent-confirm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export WAYLAND_DISPLAY=${WAYLAND_DISPLAY-wayland-1}
-[[ `swaynag --type warning --edge bottom --layer bottom --message "Allow ssh-agent?" --button-dismiss-no-terminal "Allow" "echo 1" --dismiss-button "Disallow"` == "1" ]]
+[[ `swaynag --type warning --edge bottom --layer bottom --message "$*" --button-dismiss-no-terminal "Allow" "echo 1" --dismiss-button "Disallow"` == "1" ]]
