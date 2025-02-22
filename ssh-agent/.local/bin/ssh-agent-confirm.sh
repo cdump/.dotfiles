fe9abcc6f71e74a150a073699900dcbdc5ce5472
Maxim Andreev <andreevmaxim@gmail.com>
2025-02-22 13:25:22 +0300

    ssh-agent: notify only

--- ssh-agent/.local/bin/ssh-agent-confirm.sh
+++ ssh-agent/.local/bin/ssh-agent-confirm.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 export WAYLAND_DISPLAY=${WAYLAND_DISPLAY-wayland-1}
-[[ `swaynag --type warning --edge bottom --layer bottom --message "$*" --button-dismiss-no-terminal "Allow" "echo 1" --dismiss-button "Disallow"` == "1" ]]
+notify-send 'Ssh key used'
+# [[ `swaynag --type warning --edge bottom --layer bottom --message "$*" --button-dismiss-no-terminal "Allow" "echo 1" --dismiss-button "Disallow"` == "1" ]]
