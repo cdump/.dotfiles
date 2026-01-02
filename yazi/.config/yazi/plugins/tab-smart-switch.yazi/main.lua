c0adeb2b91a1272f8180bb510542d0a6579b18af
Maxim Andreev <andreevmaxim@gmail.com>
2026-01-02 11:52:05 +0400

    yazi: update configs for v25.12.29

--- yazi/.config/yazi/plugins/tab-smart-switch.yazi/main.lua
+++ yazi/.config/yazi/plugins/tab-smart-switch.yazi/main.lua
@@ -1,13 +1,15 @@
 --- @sync entry
+
+-- https://yazi-rs.github.io/docs/tips/#smart-switch
 local function entry(_, job)
 	local cur = cx.active.current
 	for _ = #cx.tabs, job.args[1] do
-		ya.manager_emit("tab_create", { cur.cwd })
+		ya.emit("tab_create", { cur.cwd })
 		if cur.hovered then
-			ya.manager_emit("reveal", { cur.hovered.url })
+			ya.emit("reveal", { cur.hovered.url })
 		end
 	end
-	ya.manager_emit("tab_switch", { job.args[1] })
+	ya.emit("tab_switch", { job.args[1] })
 end
 
 return { entry = entry }
