78378a212d7b7ed15c2b134a635b8d3b988e835a
Maxim Andreev <andreevmaxim@gmail.com>
2026-01-16 14:53:39 +0400

    nvim: add markview.nvim

---
+++ vim/.config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,11 @@
+return {
+  'OXY2DEV/markview.nvim',
+  lazy = false,
+  keys = {
+    { '<leader>M', '<Cmd>Markview<CR>',        desc = 'Toggle Markview' },
+    { 'x',         '<Cmd>Checkbox toggle<CR>', desc = 'Toggle Checkbox', mode = { 'n', 'v' } },
+  },
+  config = function()
+    require('markview.extras.checkboxes').setup()
+  end,
+}
