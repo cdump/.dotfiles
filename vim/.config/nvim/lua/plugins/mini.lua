8e055b14d8332aec5d7074faba2df7cc13fd8312
Maxim Andreev <andreevmaxim@gmail.com>
2026-05-18 12:55:51 +0400

    vim: replace better-whitespaces with mini.trailspace

--- vim/.config/nvim/lua/plugins/mini.lua
+++ vim/.config/nvim/lua/plugins/mini.lua
@@ -65,5 +65,16 @@ return {
         end
       },
     }
+
+    require('mini.move').setup {}
+
+    require('mini.trailspace').setup {}
+    vim.api.nvim_set_hl(0, 'MiniTrailspace', { bg = '#d78787' })
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      callback = function()
+        require('mini.trailspace').trim()
+        require('mini.trailspace').trim_last_lines()
+      end,
+    })
   end
 }
